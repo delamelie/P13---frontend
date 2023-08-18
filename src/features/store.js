53fd8f510ebb4496c851a6e501cd45b7cf1490d0
Amélie de Lima <amelie.delima@hotmail.fr>
2023-08-18 12:13:08 +0200

    Blacklist user in redux-persist

--- src/features/store.js
+++ src/features/store.js
@@ -15,19 +15,61 @@ import authReducer from "./auth/authSlice";
 import userReducer from "./user/userSlice";
 import updateReducer from "./update/updateSlice";
 
-const persistConfig = {
+// const persistConfig = {
+//   key: "root",
+//   version: 1,
+//   storage,
+// };
+
+// const rootReducer = combineReducers({
+//   auth: authReducer,
+//   user: userReducer,
+//   update: updateReducer,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     user: userReducer,
+//     update: updateReducer,
+//   },
+// });
+
+// export default store;
+
+const rootPersistConfig = {
   key: "root",
   version: 1,
   storage,
 };
 
+const userPersistConfig = {
+  key: "user",
+  storage,
+  blacklist: ["isLoggedIn", "user"],
+};
+
 const rootReducer = combineReducers({
   auth: authReducer,
-  user: userReducer,
+  user: persistReducer(userPersistConfig, userReducer),
   update: updateReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
